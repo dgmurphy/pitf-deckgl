a4a9fee45029219e4ac06d4c90867bcbaae26d8d
Doug Murphy <dmurphy@incadencecorp.com>
2018-06-22 14:30:27 -0400

    delta from acled version

---
+++ components/data-files.js
@@ -0,0 +1,16 @@
+// Input Data File Names
+const DATA_DIR="./data/";
+const DataFiles=[
+    DATA_DIR + "pitf2008.out.txt",
+    DATA_DIR + "pitf2009.out.txt",
+    DATA_DIR + "pitf2010.out.txt",
+    DATA_DIR + "pitf2011.out.txt",
+    DATA_DIR + "pitf2012.out.txt",
+    DATA_DIR + "pitf2013.out.txt",
+    DATA_DIR + "pitf2014.out.txt",
+    DATA_DIR + "pitf2015.out.txt",
+    DATA_DIR + "pitf2016.out.txt",
+    DATA_DIR + "pitf2017.out.txt"
+];
+
+export { DataFiles };
